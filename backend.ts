14dee1fa8a979bd965ac5d775fc13eaf0aad7a7a
Daniel Scislowski <mr.scislowski@gmail.com>
2024-09-29 18:10:33 -0500

    serve frontend from backend

--- backend.ts
+++ backend.ts
@@ -3,6 +3,8 @@ import * as express from "express";
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("dist")); // serves up the frontend
+
 app.use("/api/hello", (req, res) => {
   res.send("hello!");
 });
